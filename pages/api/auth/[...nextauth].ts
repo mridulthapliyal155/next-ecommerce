5e4e6250ea0158cab8d41a2498ff8338c3afc0b6
mridul <thapliyamridul19@gmail.com>
2023-09-20 17:40:02 +0530

    initial commit

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth"
+import GoogleProviders from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export default NextAuth({
+    providers: [
+      GoogleProviders({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      }),
+    ],
+    adapter: PrismaAdapter(prisma),
+        // Add Another Provider
+})
